001342b990ab33b34dab02e8c5be02dad0d29295
ficnawode <81037885+ficnawode@users.noreply.github.com>
2022-05-11 10:16:49 +0200

    Revert "intellij fix"
    This reverts commit 8316efc79a3079d7654b91fe49a8fc563a8d6776.

---
+++ src/Main/Game.java
@@ -0,0 +1,59 @@
+package Main;
+
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+
+import ResManagement.SoundManager;
+
+public class Game {
+	private GameWindow gameWindow;
+	private GamePanel gamePanel;
+	private MenuPanel menuPanel;
+	private SettingsPanel settingsPanel;
+	private PausePanel pausePanel;
+	private JPanel cardPanel;
+	private CardLayout cl;
+	private int loudness = 50;//between 0 and 100;
+	
+	private SoundManager soundManager;
+	
+	public Game() {
+		cardPanel = new JPanel();
+		gameWindow = new GameWindow();
+		gameWindow.add(cardPanel);
+		
+		cl = new CardLayout();
+		cardPanel.setLayout(cl);
+		
+		menuPanel = new MenuPanel(this);		
+		cardPanel.add(menuPanel, "menu");
+		
+		gamePanel = new GamePanel(this);
+		cardPanel.add(gamePanel, "game");
+		
+		settingsPanel = new SettingsPanel(this);		
+		cardPanel.add(settingsPanel, "settings");
+		
+		pausePanel = new PausePanel(this);
+		cardPanel.add(pausePanel, "pause");
+		
+		soundManager = new SoundManager();
+
+		
+		cl.show(cardPanel, "menu");
+
+		//gamePanel.requestFocus();
+	}
+	
+	public void changeToCard(String cardName) {
+		cl.show(cardPanel, cardName);
+	}
+	
+	public SoundManager getSoundManager() {
+		return soundManager;
+	}
+	
+	public GameWindow getGameWindow() {
+		return gameWindow;
+	}
+}
